896b6cebfc8cd59864e687555630f307944fb0ba
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-21 17:46:05 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
   new_numbers = Array.new
-  array.reverse.each {|number| new_numbers.insert(0, (number**2))}
+  array.each {|number| new_numbers.push(0, (number**2))}
   new_numbers
 end
