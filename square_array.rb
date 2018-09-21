6edfbe3cb278d1aa8491f903bf6702bb6cf59cf6
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-21 17:05:59 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  squared_numbers = Array.new
+  array.each {|number|}
+  squared_numbers.insert(0, "#{number}")
+  squared_numbers.insert(1, "#{number}")
+  squared_numbers.insert(2, "#{number}")
+  squared_numbers
+  end
 end
